7cbab174492afe17255bb3254884ff3c9e266589
Alexander K <alexander.k.2000@web.de>
2022-02-25 10:17:36 +0100

    added first render for backlog

---
+++ js/backlog.js
@@ -0,0 +1,10 @@
+const LOCAL_BACKEND_KEY = 'allTasks';
+
+
+function renderBacklog(){
+    document.getElementById('mainbody').innerHTML = 
+    `
+        <h2>Backlog</h2>
+        <br>Wenn ihr das lesen könnt hats geklappt!
+    `;
+}
