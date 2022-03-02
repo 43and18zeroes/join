69fbe90b8902b0e1504d18bbef7e0da9338f0395
HW <a.k_windsor@protonmail.com>
2022-03-02 15:43:53 +0100

    getTime function changed to LocaleTime

--- js/addtask.js
+++ js/addtask.js
@@ -1,3 +1,5 @@
+setURL('http://gruppe-189.developerakademie.net/smallest_backend_ever');
+
 let profiles = [{
     'name': 'Addy W',
     'email': 'soundso@email.com'
@@ -15,8 +17,62 @@ let profiles = [{
 let allTasks = [];
 
 function renderAddTask() {
-  document.getElementById('mainbody').innerHTML = '';
-  document.getElementById('mainbody').innerHTML = `
+  renderHTML();
+}
+
+/**
+ * This function is used to create a task and put the data into a JSON.
+ */
+function createTask() {
+  let time = new Date();
+  let title = document.getElementById('title');
+  let date = document.getElementById('date');
+  let category = document.getElementById('category');
+  let urgency = document.getElementById('urgency');
+  let description = document.getElementById('description');
+  let assignedto = document.getElementById('assigendto');
+
+  let task = {
+    'id': time.toLocaleTimeString(),
+    'title': title.value,
+    'date': date.value,
+    'category': category.value,
+    'urgency': urgency.value,
+    'description': description.value,
+  };
+
+  allTasks.push(task);
+  console.log(task)
+
+  backend.setItem('allTasks', JSON.stringify(allTasks))
+
+  clearForm();
+}
+/**
+ * this function clears the Form
+ */
+function clearForm() {
+  title.value = '';
+  date.value = '';
+  category.value = '';
+  urgency.value = '';
+  description.value = '';
+}
+
+function chooseAssignedTo() {
+  let profile = document.getElementById('profile');
+  let profiles = document.getElementById('profiles');
+}
+
+async function init() {
+  await downloadFromServer();
+  allTasks = JSON.parse(backend.getItem('allTasks')) || [];
+}
+/**
+ * @returns all the rendered HTML elements
+ */
+function renderHTML() {
+  return document.getElementById('mainbody').innerHTML = `
   <header>
     <h2>All Tasks</h2>
     <span class="smallText">Learning Management System Project</span>
@@ -63,56 +119,14 @@ function renderAddTask() {
     <div class="form-control description-section">
       <label for="profile">Assigned To</label>
       <div id="profiles" class="profiles">
-        <img id="profile" class="profile-img" src="../img/imgAddy.jpg">
-        <img class="profile-img icon" src="../img/icons8-plus.png">
+        <img  class="profile-img" src="../img/imgAddy.jpg">
+        <img onclick="chooseAssignedTo()" id="profile" class="profile-img icon" src="../img/icons8-plus.png">
       </div>
       <div class="buttons">
-        <button onclick="cancel()" id="cancel">Cancel</button>
+        <button onclick="clearForm()" id="cancel">Cancel</button>
         <button onclick="createTask()" id="createTask">Create Task</button>
       </div>
     </div>
   </div>
  `;
-}
-
-/**
- * This function is used to create a task and put the data into a JSON.
- */
-function createTask() {
-
-  let title = document.getElementById('title');
-  let date = document.getElementById('date');
-  let category = document.getElementById('category');
-  let urgency = document.getElementById('urgency');
-  let description = document.getElementById('description');
-  let time = new Date();
-
-
-  let task = {
-    'id': time.getTime(),
-    'title': title.value,
-    'date': date.value,
-    'category': category.value,
-    'urgency': urgency.value,
-    'description': description.value
-  };
-
-  allTasks.push(task);
-  console.log(task)
-
-  backend.setItem('allTasks', JSON.stringify(allTasks))
-}
-
-function cancel() {
-  title.value = '';
-  date.value = '';
-  category.value = '';
-  urgency.value = '';
-  description.value = '';
-  asignedto.value = '';
-}
-
-async function init() {
-  await downloadFromServer();
-  allTasks = JSON.parse(backend.getItem('allTasks')) || [];
 }
