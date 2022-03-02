a7c905f0bff752b9fb62db413e01484cea603bac
Alexander K <alexander.k.2000@web.de>
2022-03-02 19:06:55 +0100

    added first backlog insertions

--- js/backlog.js
+++ js/backlog.js
@@ -1,16 +1,33 @@
 setURL('http://gruppe-189.developerakademie.net/smallest_backend_ever');
 
 
-function renderBacklog() {
-    init();
+async function renderBacklog() {
+    await init();
     document.getElementById('mainbody').innerHTML =
         `
         <h2>Backlog</h2>
-        <br>Wenn ihr das lesen könnt hats geklappt!
-    `;
+        
+        <div id="task-field">
+            <div id="task-title">
+                Titel: ${allTasks[0]['title']}
+            </div>
+            <div id="task-category">
+                Kategorie: ${allTasks[0]['category']}
+                </div>
+            <div id="task-description">
+                Beschreibung: ${allTasks[0]['description']}
+            </div>
+        </div>
+        `;
 }
 
 async function init() {
     await downloadFromServer();
     allTasks = JSON.parse(backend.getItem('allTasks')) || [];
+}
+
+
+function generateFrontend(){
+
+
 }
